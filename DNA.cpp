abe4a41b1d5e1282a22eb0055bc841b455984f32
Steven Dellamore <dellamoresteven@gmail.com>
2021-04-04 22:00:24 -0500

    here

--- DNA.cpp
+++ DNA.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <iostream>
+#include <vector>
 
 enum op { Plus = 0, Minus = 1, Mul = 2, Div = 3, If = 4, Constant = 5, Feature = 6 };
 
@@ -25,9 +26,55 @@ class DNA {
         }
         ~DNA() {}
 
+        template<typename T>
+        float classify(T FeatureTranslator) {
+        }
+
+        template<typename T>
+        float fitnessTest(T correctnessFunc) {
+            if(classifications.size() == 0) {
+                std::cout << "DNA: Classifications is 0" << std::endl;
+                return -1;
+            }
+            int numCorrect = 0;
+            for(const auto & c : classifications) {
+                if(correctnessFunc(c)) {
+                    numCorrect += 1;
+                }
+            }
+            fitness = float(numCorrect) / float(classifications.size());
+            return fitness;
+        }
+
+        float getFitness() {
+            return fitness;
+        }
+
     private:
+        std::vector<float> classifications;
+        float fitness;
 };
+
+void buildRandomDNA() {
+
+}
 
 int main() {
     DNA dna;
+    auto testClassify = [](int c){
+        switch(c) {
+            case 0:
+                return 0;
+            case 1:
+                return 10;
+            case 2:
+                return 20;
+            case 3:
+                return 30;
+            default:
+                return -5;
+        }
+    };
+    dna.classify(testClassify);
+    dna.fitnessTest([](float x){return false;});
 }

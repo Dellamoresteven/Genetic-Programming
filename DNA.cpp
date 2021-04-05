099b6e525f5a96c9967217e617025677561808cb
Steven Dellamore <dellamoresteven@gmail.com>
2021-04-05 16:14:54 -0500

    what

--- DNA.cpp
+++ DNA.cpp
@@ -1,44 +1,121 @@
 #include <stdlib.h>
 #include <iostream>
+#include <time.h>
 #include <vector>
 
 enum op { Plus = 0, Minus = 1, Mul = 2, Div = 3, If = 4, Constant = 5, Feature = 6 };
 
-struct nucleotide {
+struct gene {
     bool root = false;
     op type;
     float value;
-    nucleotide * l = nullptr;
-    nucleotide * m = nullptr; // Will be null if operator is if
-    nucleotide * r = nullptr;
+    gene * l = nullptr;
+    gene * m = nullptr; // Will be null if operator is if
+    gene * r = nullptr;
 };
 
 class DNA {
     public:
-        nucleotide * gRoot;
+        gene * gRoot;
 
         DNA() {
-            gRoot = new nucleotide();
+            gRoot = new gene();
+            gRoot->root = true;
         }
 
-        DNA(nucleotide * pRoot) {
+        DNA(gene * pRoot) {
             gRoot = pRoot;
         }
-        ~DNA() {}
 
-        template<typename T>
-        float classify(T FeatureTranslator) {
+        ~DNA() {
+            // @TODO Clean all other genes starting from root.
+        }
+};
+
+
+class Agent {
+    private:
+        DNA * dna;
+        float fitness;
+        std::vector<float> classifications;
+
+
+    public:
+        Agent() {}
+        Agent(DNA * dna) {
+            dna = dna;
+            srand((unsigned) time(NULL));
         }
 
+        /**
+         * Builds a random DNA strain
+         */
+        template <typename T>
+        void setRandomDNAStrain(T randomizationValues) {
+            auto rec = [&](gene * currGene) -> auto {
+                float randNum = static_cast<float> (rand()) / (static_cast<float>(RAND_MAX));
+                gene newGene = randomizationValues(randNum);
+                switch(newGene.type) {
+                    case Plus:
+                        break;
+                    case Minus:
+                        break;
+                    case Mul:
+                        break;
+                    case Div:
+                        break;
+                    case If:
+                        break;
+                    default:
+                        std::cout << "Something went wrong" << std::endl;
+                        exit(1);
+                }
+                rec(currGene->l);
+                rec(currGene->m);
+                rec(currGene->r);
+                return 0;
+            };
+            DNA * ret = new DNA();
+            rec(ret->gRoot);
+            dna = ret;
+        }
+
+        /**
+         * Resets all vars in class
+         */
+        void reset() {
+            classifications.clear();
+            fitness = 0;
+        }
+
+        /**
+         * Classify's each object
+         *
+         * @param featureTranslator is a function that takes in a feature int and returns
+         *        the real feature value.
+         */
+        template <typename T>
+        void classification(T featureTranslator) {
+        }
+
+        /**
+         * Calculates the fitness based on the current classifications. The score is
+         * equal to right answers over wrong answers.
+         *
+         * @param: A function that takes in a float and returns a boolean telling us
+         *         if we got it wrong (false) or right (true).
+         *
+         * @return a fitness score (float)
+         */
         template<typename T>
-        float fitnessTest(T correctnessFunc) {
+        float calcSimpleFitness(T fitnessTest) {
             if(classifications.size() == 0) {
                 std::cout << "DNA: Classifications is 0" << std::endl;
                 return -1;
             }
             int numCorrect = 0;
             for(const auto & c : classifications) {
-                if(correctnessFunc(c)) {
+                if(fitnessTest(c)) {
                     numCorrect += 1;
                 }
             }
@@ -49,18 +126,17 @@ class DNA {
         float getFitness() {
             return fitness;
         }
-
-    private:
-        std::vector<float> classifications;
-        float fitness;
 };
 
-void buildRandomDNA() {
-
+/**
+ * DNA print override
+ */
+std::ostream& operator<<(std::ostream& os, const DNA& dt) {
 }
 
+
 int main() {
-    DNA dna;
+    Agent * a = new Agent();
     auto testClassify = [](int c){
         switch(c) {
             case 0:
@@ -75,6 +151,5 @@ int main() {
                 return -5;
         }
     };
-    dna.classify(testClassify);
-    dna.fitnessTest([](float x){return false;});
+    a->calcSimpleFitness([](float x){return false;});
 }

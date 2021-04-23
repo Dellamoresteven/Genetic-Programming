252822b0ee71d2793d68c2e9401e56de6ba3abe4
ColinLi33 <Colin8tor1206@gmail.com>
2021-04-23 15:36:43 -0500

    MUTATE

---
+++ mutate.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <limits>
+#include <memory>
+#include <functional>
+
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+
+ int   populationSize = 200;  // Number of agents per round
+    int   selectionSize  = 10;    // Number of agents in the tournament selection
+    int   maxDepth       = 10;    // Max Depth of classification tree
+    int   maxGenerations = 100000; // Max number of generations
+    int   numBreed       = 10;    // Number of new agents during breeding
+    int   numAdopt       = 20;    // Number of new agents to adopt
+    float mutationRate   = .1;  // % mutation rate of DNA
+    float OpChance       = 0.40; // Chance of adding an operator to the DNA
+    float constChance    = 0.1; // Chance of adding a constant to the DNA
+    float featureChance  = 0.5; // Chance of adding a feature to the DNA
+    int   maxConst       = 10;  // Max const
+    int   bestFitness    = 0; // Best fitness of all gens
+
+
+enum op { Plus = 0, Minus = 1, Mul = 2, Div = 3, If = 4, Constant = 5, Feature = 6 };
+
+#define print(x) std::cout << #x << " : " << x << std::endl;
+
+
+
+struct gene {
+    gene(bool isRoot, op o, float val) {
+        root  = isRoot;
+        type  = o;
+        value = val;
+    }
+
+    bool root = false;
+    op type;
+    float value;
+    std::unique_ptr<gene> l = NULL;
+    std::unique_ptr<gene> m = NULL; // Will not be null if operator is if
+    std::unique_ptr<gene> r = NULL;
+};
+
+gene* mutFunc(gene* g, int gDepth){
+    float randNum = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+    std::function<gene*(int)> makeRandomGene = [&](int depth){
+        float opRand = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+        int value = 0;
+        if(depth == maxDepth){
+            while(opRand < .4){
+                opRand = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+            }
+        }
+        print(opRand);
+        if(opRand < .32){ //+-*/
+            g->l.reset(makeRandomGene(depth+1));
+            g->r.reset(makeRandomGene(depth+1));
+        } else if(opRand > .32 && opRand < .4){ //if
+            g->l.reset(makeRandomGene(depth+1));
+            g->m.reset(makeRandomGene(depth+1));
+            g->r.reset(makeRandomGene(depth+1));
+        } else if(opRand > .4 && opRand < .5){ //constant
+            value = rand() % maxConst + 1;
+        } else if(opRand > .5){ //feature
+            value = rand() % 23;
+        }
+        return new gene(g->root,static_cast <op>(opRand),value);
+    };
+    if(randNum < mutationRate){
+        g = makeRandomGene(gDepth);
+    }
+    return g;
+}
+
+
+int main(){
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+    gene * fakeGene = new gene(false, op::Plus,2);
+    rand();
+    fakeGene = mutFunc(fakeGene, 0);
+}
